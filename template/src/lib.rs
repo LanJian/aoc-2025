db30e5fcf4f96e70f00d9ca8ff549f591c0c7e42
Jack Huang <jackhxs@gmail.com>
2025-11-29 17:52:50 -0800

    fix: fix some template issues

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -48,7 +48,7 @@ mod tests {
     #[test]
     fn example() {
         let input = std::fs::read_to_string("example.txt").expect("Unable to load input");
-        let solution = {{project-name|upper_camel_case}}::solve(input).unwrap();
+        let solution = {{project-name|upper_camel_case}}::solve(&input).unwrap();
         assert_eq!(solution, Solution::new(0, 0));
     }
 }
